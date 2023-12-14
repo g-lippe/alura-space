68e8f60b2e36de45b2041c8ba022e5153e9fb1cc
Gabriel Felippe <gabrielalexfelippe@gmail.com>
2023-12-14 15:58:03 -0300

    Parity Aula

---
+++ src/componentes/Populares/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import fotosPopulares from './fotos-populares.json'
+
+export default function Populares() {
+  return (
+    <aside>
+      <h2>Populares</h2>
+      <ul>
+
+      </ul>
+    </aside>
+  )
+}
